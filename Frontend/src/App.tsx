91909e95e604b104fd696e0e10e4ada7c29fa720
ansaralikunju123 <ansarpanoor311@gmail.com>
2026-03-02 22:27:47 +0530

    in this commit setup the frontend folder structure

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold text-blue-600">
+      Occasio Frontend Ready 🚀
+    </h1>
+    </div>
+  )
+}
+
+export default App
