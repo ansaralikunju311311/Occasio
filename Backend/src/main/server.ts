41f094880b0d43991b5cbd3c7b8514410d294322
ansaralikunju123 <ansarpanoor311@gmail.com>
2026-03-05 12:51:02 +0530

    in this commit i do the remove otp after the expire time

--- Backend/src/main/server.ts
+++ Backend/src/main/server.ts
@@ -5,10 +5,12 @@ import "../shared/config/env.js"
 import app from '../main/app.js'
 
 import { initializaApp } from '../shared/loaders/index.js'
+import { clearExpiredOtpJob } from "../shared/loaders/index.js"
 const startServer = async () : Promise<void>=>{
     
     try {
-        await initializaApp()
+        await initializaApp();
+        clearExpiredOtpJob()
         app.listen(3001,()=>{
         console.log(`the server is running propelrly`)
     })
--- Backend/src/shared/loaders/index.ts
+++ Backend/src/shared/loaders/index.ts
@@ -1,7 +1,32 @@
 import { connectDateBase } from "../config/database.js"
 export const initializaApp = async():Promise<void>=>{
-
  await connectDateBase();
- 
-
 }
+import cron from "node-cron";
+import { UserModel } from "../../modules/auth/infrastructure/database/user.model.js";
+
+export const clearExpiredOtpJob = () => {
+  cron.schedule("* * * * *", async () => {
+    try {
+      const now = new Date();
+
+      await UserModel.updateMany(
+        { otpExpires: { $lt: now } },
+        {
+          $set: {
+            otp: null,
+           
+             
+       otpExpires:null,
+       otpType:null,
+      otpSendAt:null
+          }
+        }
+      );
+
+      console.log("Expired OTPs cleared");
+    } catch (error) {
+      console.error("Cron job error:", error);
+    }
+  });
+};
